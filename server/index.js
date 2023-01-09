54b52554eac624fa7241df9bf86379fe04f89924
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-08 21:07:25 -0500

    static

--- server/index.js
+++ server/index.js
@@ -13,9 +13,26 @@ app.use(router)
 
 router.get("/hello", (req, res) => {
     res.send({status: "Express on Vercel"});
-  });
-  
-  // Initialize server
+});
+
+
+
+
+
+const path = require('path')
+const reactPath = path.resolve(__dirname, "..", "client", "public");
+app.use(express.static(reactPath));
+
+// for all other routes, render index.html and let react router handle it
+app.get("*", (req, res) => {
+  res.sendFile(path.join(reactPath, "index.html"));
+});
+
+
+
+
+
+// Initialize server
 app.listen(5000, () => {
     console.log("Server running on port 5000.");
 });
