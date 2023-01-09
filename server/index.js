465b5411213f709c2ad2583ed51dc1e3c17300da
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-08 23:50:14 -0500

    r

--- server/index.js
+++ server/index.js
@@ -28,13 +28,11 @@ const path = require('path')
 // });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("/*", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
     });
 }
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
 // Initialize server
