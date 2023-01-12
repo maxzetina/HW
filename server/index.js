aac597c4655305f98e58bce741c09fd7671d16b8
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-11 19:55:18 -0500

    api file

--- server/index.js
+++ server/index.js
@@ -2,42 +2,39 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require("mongoose");
+const api = require("./api");
+
 require("dotenv").config();
 
-// const path = require('path')
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-
 const mongoConnectionURL = process.env.MONGODB_URI;
-// // TODO change database name to the name you chose
 const databaseName = "employees";
-mongoose.set('strictQuery', true);
 
-// connect to mongodb
-mongoose
-  .connect(mongoConnectionURL, {
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoConnectionURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: databaseName,
   })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
+  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`)
+);
 
 app.use(cors())
 app.use(express.json())
-const router = express.Router();
-app.use("/api", router)
+// const router = express.Router();
+// app.use("/api", router)
+app.use("/api", api)
 
+// router.get("/hello", (req, res) => {
+//     res.send({status: "Express on Vercel"});
+// });
+// const emp = require("./models/employee");
 
-router.get("/hello", (req, res) => {
-    res.send({status: "Express on Vercel"});
-});
-const emp = require("./models/employee");
-
-router.get("/emps", (req, res) => {
-    emp.find({name : req.query.name}).then((userFound) => {
-        console.log(userFound);
-        res.send(userFound)})
-});
+// router.get("/emps", (req, res) => {
+//     emp.find({name : req.query.name}).then((userFound) => {
+//         console.log(userFound);
+//         res.send(userFound)})
+// });
 
 
 const path = require('path')
