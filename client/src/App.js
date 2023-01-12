f0af95e7e75c2d86ffea677620ae7cf80303e4e2
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-11 20:44:51 -0500

    utilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,7 @@
 import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
-
-
-
-
-function formatParams(params) {
-  // iterate of all the keys of params as an array,
-  // map it to a new array of URL string encoded key,value pairs
-  // join all the url params using an ampersand (&).
-  return Object.keys(params)
-    .map((key) => key + "=" + encodeURIComponent(params[key]))
-    .join("&");
-}
-
-// convert a fetch result to a JSON object with error handling for fetch and json errors
-function convertToJSON(res) {
-  if (!res.ok) {
-    throw `API request failed with response status ${res.status} and text: ${res.statusText}`;
-  }
-
-  return res
-    .clone() // clone so that the original is still readable for debugging
-    .json() // start converting to JSON object
-    .catch((error) => {
-      // throw an error containing the text that couldn't be converted to JSON
-      return res.text().then((text) => {
-        throw `API request's result could not be converted to a JSON object: \n${text}`;
-      });
-    });
-}
-
-// Helper code to make a get request. Default parameter of empty JSON Object for params.
-// Returns a Promise to a JSON Object.
-function get(endpoint, params = {}) {
-  const fullPath = endpoint + "?" + formatParams(params);
-  console.log(fullPath);
-  return fetch(fullPath)
-    .then(convertToJSON)
-    .catch((error) => {
-      // give a useful error message
-      throw `GET request to ${fullPath} failed with error:\n${error}`;
-    });
-}
-
-
-
-
-
-
-
+import { get } from './utilities';
 
 function App() {
 
