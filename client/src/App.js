e4e73c338f161f9800955c358d35357fb309f916
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-12 13:04:09 -0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { get } from './utilities';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { PrimaryButton } from '@fluentui/react';
-
-
+import { NavBasicExample } from './components/Nav';
+import Assignments from "./components/Assignments";
+import Overview from "./components/Overview";
 function App() {
 
   const [example, setExample] = useState('')
@@ -28,25 +30,41 @@ function App() {
     };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {example.status}
-          {emp}
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <PrimaryButton onClick={_alertClicked} text="Click Me"/>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Overview />}/>
+        <Route path="/assignments" element={<Assignments />} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <div>
+  //         {example.status}
+  //         {emp}
+  //       </div>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //       <PrimaryButton onClick={_alertClicked} text="Click Me"/>
+
+  //       {/* {NavBasicExample()} */}
+  //       <Router>
+  //         <Overview path='/' />
+  //         <Assignments path='/assignments' />
+  //         {/* <NotFound default /> */}
+  //       </Router>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
