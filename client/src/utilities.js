4c798b30e09fba5be5ec44f64acae69b70acfbfe
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-17 17:25:46 -0500

    last try

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -4,7 +4,7 @@
     e.g. get('/api/foo', { bar: 0 }).then(res => console.log(res))
 */
 
-const BASE_URL = "https://trying-maxzetina.vercel.app";
+// const BASE_URL = "https://trying-maxzetina.vercel.app";
 // const BASE_URL = "http://localhost:5000";
 
 
@@ -38,7 +38,8 @@ function formatParams(params) {
   // Helper code to make a get request. Default parameter of empty JSON Object for params.
   // Returns a Promise to a JSON Object.
   export function get(endpoint, params = {}) {
-    const fullPath = BASE_URL + endpoint + "?" + formatParams(params);
+    // const fullPath = BASE_URL + endpoint + "?" + formatParams(params);
+    const fullPath = endpoint + "?" + formatParams(params);
     return fetch(fullPath)
       .then(convertToJSON)
       .catch((error) => {
