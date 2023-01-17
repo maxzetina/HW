0dc8599279d482ba3da83164a1a3e63cc27115e7
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-01-17 17:16:18 -0500

    build in sever

--- server/index.js
+++ server/index.js
@@ -30,9 +30,9 @@ mongoose.connect(mongoConnectionURL, {
 //         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
 //     });
 // }
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static('build'));
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile("build/index.html");
 });
 
 
